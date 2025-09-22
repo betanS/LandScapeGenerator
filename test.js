cb055bb55d1c1ad23d93d6397ffa237c23941bb6
betanS <betancorta666@gmail.com>
2025-09-22 10:48:21 +0100

    testing with js

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
- 
+var carteles = prompt("carteles");
+document.writeln(carteles);
+
+var numPuerta = prompt("numPuerta");
+document.writeln(numPuerta);
+var canPuerta = prompt("canPuerta");
+document.writeln(canPuerta);
+
+var escaparates = prompt("escaparates");
+document.writeln(escaparates);
+var escaparatesOfer = prompt("escaparatesOfer");
+document.writeln(escaparatesOfer);
+
+var carteles = prompt("carteles");
+document.writeln(carteles);
